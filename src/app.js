981495d0634bf6175a8ad71fbdfda40432dc5608
Rahul Ravella <rahulravella@Rahuls-MacBook-Pro.local>
2023-06-05 15:37:50 -0500

    minor styling header

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Helmet from 'react-helmet'
 import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-
 import {
   ErrorBoundary,
   Facet,
@@ -16,6 +15,7 @@ import {
 } from "@elastic/react-search-ui";
 import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import "./styles.css"
 
 import {
   buildAutocompleteQueryConfig,
